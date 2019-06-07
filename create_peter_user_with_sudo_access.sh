4122b52045d6b35583b81a69161b0db4ab7240e6
Peter G. Williams <petergwilliams@hotmail.com>
2019-06-07 12:23:52 +0100

    Change user from 'peter' to 'peterw'

--- create_peter_user_with_sudo_access.sh
+++ create_peter_user_with_sudo_access.sh
@@ -2,13 +2,13 @@
 
 MY_SHELL=$( which bash )
 
-/usr/sbin/useradd -c 'Peter G. Williams <petergwilliams at hotmail.com>' -s ${MY_SHELL} -m peter
-cat >/etc/sudoers.d/peter-ALL <<EOF
-peter     ALL=(ALL:ALL) NOPASSWD: ALL
+/usr/sbin/useradd -c 'Peter G. Williams <petergwilliams at hotmail.com>' -s ${MY_SHELL} -m peterw
+cat >/etc/sudoers.d/peterw-ALL <<EOF
+peterw     ALL=(ALL:ALL) NOPASSWD: ALL
 EOF
-chmod 0440 /etc/sudoers.d/peter-ALL
-mkdir ~peter/.ssh
-chmod 700 ~peter/.ssh
-curl https://raw.githubusercontent.com/pgwilliams/ssh_access/master/petergwilliams_rsa.pub >~peter/.ssh/authorized_keys
-chmod 400 ~peter/.ssh/authorized_keys
-chown -R peter ~peter/.ssh
+chmod 0440 /etc/sudoers.d/peterw-ALL
+mkdir ~peterw/.ssh
+chmod 700 ~peterw/.ssh
+curl https://raw.githubusercontent.com/pgwilliams/ssh_access/master/petergwilliams_rsa.pub >~peterw/.ssh/authorized_keys
+chmod 400 ~peterw/.ssh/authorized_keys
+chown -R peterw ~peterw/.ssh
